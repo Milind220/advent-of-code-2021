b505692350eff4dace31503b9755ea4eda03d662
Milind Sharma <milindsharma8@gmail.com>
2021-12-04 18:26:33 +0800

    Add day 2 solution and input

---
+++ day_2/2_1.py
@@ -0,0 +1,21 @@
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+hor_pos: int = 0
+depth_pos: int = 0
+
+for line in lines:
+    direction, magnitude = line.split()
+    magnitude = int(magnitude)
+
+    if direction == 'horizontal':
+        hor_pos += magnitude
+    
+    elif direction == 'up':
+        depth_pos -= magnitude
+    
+    else:
+        depth_pos += magnitude
+    
+final_pos: int = hor_pos * depth_pos
+print(final_pos)
