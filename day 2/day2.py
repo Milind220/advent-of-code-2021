2df919eee7cebf3d241f8dbff02f216654ebc462
Milind Sharma <milindsharma8@gmail.com>
2021-12-04 13:39:42 +0800

    Add day 1 part 2  solutions

---
+++ day 2/day2.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+with open('input.txt', 'r') as f:
+    numbers: List[float] = list(map(float, f.readlines()))
+
+sum_list: List[float] = []
+
+for i, num in enumerate(numbers):
+
+    if (i > 0) and (i < len(numbers) - 1):
+        numsum: float = num + numbers[i-1] + numbers[i+1]
+        sum_list.append(numsum)
+
+counter: int = 0
+prev_num: float = sum_list[0]
+
+for i, num in enumerate(sum_list):
+    if num > prev_num:
+        counter += 1
+            
+    prev_num = num
+
+print(counter)
