916f53eacda08abf9da7531c57171ef97352391c
Milind Sharma <milindsharma8@gmail.com>
2021-12-06 00:09:11 +0800

    Add basic components for input reading and parts

---
+++ day_4/4.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def open_file() -> List[str]:
+    with open('input.txt', 'r') as f:
+        input_data: List[str] = f.readlines()
+    return input_data
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+if __name__ == '__main__':
+    pass
