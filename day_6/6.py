6f2ead909d16764e66749f6d0a9498a8bbb4d052
Milind Sharma <milindsharma8@gmail.com>
2021-12-10 01:14:17 +0800

    Add day 6 part 1 solutions

---
+++ day_6/6.py
@@ -0,0 +1,42 @@
+"""Day 6 of Advent of Code 2021, part 1 and 2"""
+
+from typing import List
+
+
+def get_input_data(filename: str) -> List[int]:
+    with open(filename, 'r') as f:
+        data: List[int] = [int(num) for num in f.readline().split(',')]
+    return data
+
+
+def pass_day(fish_list: List[int]) -> List[int]:
+    return [num - 1 for num in fish_list]
+
+
+def reset_fish(fish_list: List[int]) -> List[int]:
+    return [6 if x == -1 else x for x in fish_list]
+
+
+def part_1(array: List[int]) -> int:
+    days: int = 80
+    for day in range(days):
+        array = pass_day(array)
+        new_fish: int = array.count(-1)
+        array = reset_fish(array)
+        if new_fish:
+            array.extend([8] * new_fish)
+    
+    return len(array)
+
+    # Answer for part 1 was 360761.
+
+
+def main():
+    raw_data: List[int] = get_input_data('input.txt')
+    ans_one = part_1(raw_data)
+    
+    print(ans_one) # 360761
+    
+
+if __name__ == '__main__':
+    main()
