9c7df0daa7aaf7e0fe6528d381c3400797716a5c
Milind Sharma <milindsharma8@gmail.com>
2021-12-10 02:05:31 +0800

    Optimise previous algorithm with numpy

--- day_6/6.py
+++ day_6/6.py
@@ -2,6 +2,8 @@
 
 from typing import List
 
+import numpy as np
+
 
 def get_input_data(filename: str) -> List[int]:
     with open(filename, 'r') as f:
@@ -9,6 +11,7 @@ def get_input_data(filename: str) -> List[int]:
     return data
 
 
+# Part 1 functions
 def pass_day(fish_list: List[int]) -> List[int]:
     return [num - 1 for num in fish_list]
 
@@ -31,15 +34,27 @@ def part_1(array: List[int]) -> int:
     # Answer for part 1 was 360761.
 
 
-def part_2(array: List[int]) -> int:
+def part_2(arr: List[int]) -> int:
+    fish_arr: np.ndarray = np.array(arr, dtype = np.int8)
+    
+    days: int = 256
+    for day in range(days):
+        print(day)
+        fish_arr = fish_arr - 1  # pass a single day
+        new_fish = np.count_nonzero(fish_arr == -1)
+        fish_arr[fish_arr == -1] = np.int8(6)
 
-    return 1
+        if new_fish:
+            fish_arr = np.append(fish_arr, 8 * np.ones(new_fish, dtype = np.int8))
+
+    return len(fish_arr)    
 
 
 def main():
-    raw_data: List[int] = get_input_data('input.txt')
-    ans_two = part_2(raw_data)
+    raw_data: List[int] = get_input_data('test.txt')
+    
     #ans_one = part_1(raw_data)
+    ans_two = part_2(raw_data)
 
     #print(ans_one) # 360761
     print(ans_two)
