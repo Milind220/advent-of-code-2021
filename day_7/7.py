95dc61e6da924ca35aa48bf4bb88c48b2b7478cc
Milind Sharma <milindsharma8@gmail.com>
2021-12-10 13:55:35 +0800

    Create skeleton for day 7 solution

---
+++ day_7/7.py
@@ -0,0 +1,23 @@
+"""Day 7, part 1 and 2 of Advent Of Code https://adventofcode.com/2021/day/7 """
+
+from typing import List
+
+import numpy as np
+
+
+def get_input_data(filename: str):
+    pass
+
+
+# Part 1 functions
+def part_1() -> int:
+    return 1
+
+
+# Explicit main
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
