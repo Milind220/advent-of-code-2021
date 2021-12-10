5cb96e553f797edd8bd855f485d7fa9b51ca81e4
Milind Sharma <milindsharma8@gmail.com>
2021-12-10 21:27:03 +0800

    Create skeleton for day 8 solutions

---
+++ day_8/8.py
@@ -0,0 +1,23 @@
+"""Day 8, part 1 and 2 of Advent Of Code https://adventofcode.com/2021/day/8 """
+
+
+from typing import List
+
+
+def get_input_data(filename: str):
+    with open(filename, 'r') as f:
+        pass
+
+
+
+# Part 1 functions
+
+
+
+# Explicit main
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
