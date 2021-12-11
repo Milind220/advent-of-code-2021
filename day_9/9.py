a4ce8e7bd262b74550952a8a5160c5dbb09c4c09
Milind Sharma <milindsharma8@gmail.com>
2021-12-11 18:46:29 +0800

    Add skeleton for day 9 solution

---
+++ day_9/9.py
@@ -0,0 +1,23 @@
+"""Day 9, part 1 and 2 of Advent Of Code https://adventofcode.com/2021/day/9 """
+
+from typing import List
+
+
+def get_input_data(filename: str) -> List[]:
+    with open(filename, 'r') as f:
+        pass
+
+
+def part_1() -> int:
+    return 1
+
+
+def main():
+
+    ans_one: int = part_1()
+
+    print(ans_one)
+
+
+if __name__ == '__main__':
+    main()
